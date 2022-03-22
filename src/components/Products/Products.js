40269cfc8230aeb3ce00fce71c05cdbcb7c972f5
masud rana <masudranaacb@gmail.com>
2022-03-22 23:18:46 +0600

    using some style

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,11 +1,18 @@
 import React from 'react';
-
+import './Products.css'
 const Products = (props) => {
     console.log(props.product)
-    const {name,img}=props.product
+    const {name,img,seller,price,ratings}=props.product
     return (
-        <div>
+        <div className='products'>
+            <div className='products-info'>
             <img src={img} alt="" />
+            <p>{name}</p>
+            <p>Price:${price}</p>
+            <p>Seller:{seller}</p>
+            <p>Ratings:{ratings} star</p>
+            </div>
+            <button className='card-btn'>Add To Card</button>
         </div>
     );
 };
